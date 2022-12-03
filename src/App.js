86543abb89ac40239d52d6aeed039372b94fd7d2
abdullahhosenakash <abdullahhosenakash@gmail.com>
2022-12-03 19:20:42 +0600

    tailwind css added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className="underline">Hello</h1>
+      <button class="btn btn-primary">Button</button>
     </div>
   );
 }
